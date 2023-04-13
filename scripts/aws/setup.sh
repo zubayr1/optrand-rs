eab33bffc59ed2daf365668ad6f03b3638fdb96a
zubayr1 <write2zubayr@gmail.com>
2023-04-13 23:31:12 +0200

    fix git clone

--- scripts/aws/setup.sh
+++ scripts/aws/setup.sh
@@ -11,7 +11,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > install-rust.sh
 bash install-rust.sh -y
 source $HOME/.cargo/env
 
-git clone https://github.com/zhtluo/randpiper-rs.git
+git clone https://github.com/zubayr1/optrand-rs.git
 cd randpiper-rs
 
 git pull
