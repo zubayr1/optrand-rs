cac04e9b77778ec3638cdf897ba75d6818556100
zubayr1 <write2zubayr@gmail.com>
2023-04-18 12:36:18 +0200

    fix more errors

--- scripts/aws/setup.sh
+++ scripts/aws/setup.sh
@@ -1,9 +1,9 @@
-# sudo pacman -Syu --noconfirm
+sudo pacman -Syu --noconfirm
 sudo apt-get update
 sudo apt install make
 sudo apt install build-essential
 
-# sudo pacman -S git --noconfirm
+sudo pacman -S git --noconfirm
 sudo apt-get install git build-utils
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > install-rust.sh
--- scripts/aws/test.sh
+++ scripts/aws/test.sh
@@ -1,2 +1,2 @@
-killall -9 node-bft
+killall -9 node-optrand
 timeout 180 ./optrand-rs/target/release/node-optrand -c ./optrand-rs/testdata/n3-f1/nodes-$1.dat -d 280 -i ./optrand-rs/ips_file > output.log
